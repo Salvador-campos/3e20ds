8fa18490addb953ed229a562397b6636231fdb78
Salvador-campos <72282258+Salvador-campos@users.noreply.github.com>
2021-01-18 01:45:13 -0600

    Delete hanoi.js

--- 2p/ej2/hanoi.js
+++
@@ -1,8 +0,0 @@
-var hanoi = function(disc,src,aux,dest){
-    if(disc > 0){
-      hanoi(disc -1,src,dest,aux);
-      console.log('Mover disco '+disc+' de '+src+' a '+dest);
-      hanoi(disc -1,aux,src,dest);
-    }
-  };
-hanoi(3,'buscar','temp','destino');
